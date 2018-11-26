e9c615021d20d464865b1d46d746dc9d1052ee3c
mael.drapier <mael.drapier@epitech.eu>
2018-11-26 16:45:45 +0100

    #12 - adding the HitboxSystem class

---
+++ Server/include/HitboxSystem.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2018
+** R-Type
+** File description:
+** HitboxSystem system class for R-Type
+*/
+
+#ifndef HITBOXSYSTEM_HPP_
+	#define HITBOXSYSTEM_HPP_
+
+namespace Ecs {
+	class HitboxSystem {
+		public:
+			HitboxSystem();
+			~HitboxSystem();
+
+		protected:
+		private:
+	};
+}
+
+#endif /* !HITBOXSYSTEM_HPP_ */
