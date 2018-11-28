a34dafda81c2555bc6b17da4b49644681ac2edbb
mael.drapier <mael.drapier@epitech.eu>
2018-11-28 14:56:42 +0100

    #15 - adding an empty HitboxSystemTests.cpp file

---
+++ Server/tests/HitboxSystemTests.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** R-Type
+** File description:
+** Hitbox unit tests
+*/
+
+#include <criterion/criterion.h>
+#include "HitboxSystem.hpp"
+
+Test(HitboxSystem, takeDamages)
+{
+	cr_assert_eq(1, 1);
+}
