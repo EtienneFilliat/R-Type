349f4b4f3d7281434ad5b1f68cfaa7816cada44e
Etienne <etienne.filliat@epitech.eu>
2018-12-05 14:22:31 +0100

    Fix compile

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 mkdir -p build; cd build && conan install .. --build=missing  -s compiler.libcxx=libstdc++11 && cmake -DCMAKE_BUILD_TYPE=Debug .. && cmake --build  . -- -j 3
 
-cp ./build/r-type_client ..
+cp ./r-type_client ..
 
-cp ./build/r-type_server ..
+cp ./r-type_server ..
